dda56074f33fcea97fa3def7e64affc6285fd7b5
mashafrancis <francismasha96@gmail.com>
2018-10-24 14:22:29 +0300

    Add UI controllers and fetch elements

--- app.js
+++ app.js
@@ -2,15 +2,78 @@
 
 // Item Controller
 const ItemCtrl = (function () {
+    // Item constructor
+    const Item = function (id, name, calories) {
+        this.id = id;
+        this.name = name;
+        this.calories = calories
+    };
 
+    // Data Structure / State
+    const data = {
+        items: [
+            {id: 0, name: 'Steak Dinner', calories: 1200},
+            {id: 1, name: 'Eggs', calories: 100},
+            {id: 2, name: 'Cookies', calories: 600}
+        ],
+        currentItem: null,
+        totalCalories: 0
+    };
+
+    return {
+        getItems: function() {
+            return data.items;
+        },
+        logData: function () {
+            return data;
+        }
+    }
 })();
 
 // UI Controller
 const UICtrl = (function () {
+    const UISelectors = {
+        itemList: '#item-list'
+    };
+
+    // Public methods
+    return {
+        populateItemList: function (items) {
+            let html = '';
+
+            items.forEach(function (item) {
+                html += `
+                <li class="collection-item" id="item-${item.id}">
+                  <strong>${item.name}: </strong> <em>${item.calories} Calories</em>
+                  <a href="" class="secondary-content">
+                    <i class="edit-item fa fa-pencil"></i>
+                  </a>
+                </li>
+                `
+            });
+
+            // Insert list items
+            document.querySelector(UISelectors.itemList).innerHTML = html;
+        }
+    }
 
 })();
 
 // App Controller
-const AppCtrl = (function (ItemCtrl, UICtrl) {
+const App = (function (ItemCtrl, UICtrl) {
+    
+    // Public methods
+    return {
+        init: function () {
+            // Fetch items from data structure
+            const items = ItemCtrl.getItems();
+
+            // Populate list with items
+            UICtrl.populateItemList(items)
+        }
+    }
 
 })(ItemCtrl, UICtrl);
+
+// Initialize App
+App.init();
